ecd197dee11d3f10452e8dfde61bb3a4cdaf6853
Lazea <lazeaai@gmail.com>
2023-06-15 21:20:21 -0400

    Implemented functional projectiles and damage system. Added fx for testing

--- Assets/Scripts/Utils/CombatUtils.cs
+++ Assets/Scripts/Utils/CombatUtils.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,52 +7,53 @@ public static class CombatUtils
     public static void ApplyDamage(Damage damage, IDamageable damageable)
     {
         // Check GUID before applying damage
-        if (damageable.CheckGUIDIsInBuffer(damage.guid))
+        if (!damageable.CheckGUIDIsInBuffer(damage.guid))
             return;
 
-        // Apply shield damage
-        int damageAmount = Mathf.RoundToInt(damage.amount * damage.shieldMultiplier);
-        if (damageable.Shield > damageAmount)
+        int shieldDamage = Mathf.RoundToInt(damage.amount * damage.shieldMultiplier);
+
+        damageable.Shield -= shieldDamage;
+        if (damageable.Shield <= 0)
         {
-            damageable.Shield -= damageAmount;
-            damageAmount = 0;
-        }
-        else
-        {
-            damageAmount -= damageable.Shield;
+            int healthDamage = Mathf.RoundToInt(Mathf.Abs(damageable.Shield) / damage.shieldMultiplier);
             damageable.Shield = 0;
 
-            damageAmount = Mathf.RoundToInt(damageAmount / damage.shieldMultiplier);
+            damageable.Health -= healthDamage;
+            if (damageable.Health <= 0)
+                damageable.Health = 0;
         }
-
-        // Apply remaining damage to health
-        int damageDealt = (damageable.Health < damageAmount) ? 
-            damageable.Health : damageAmount;
-        damageable.Health -= damageDealt;
     }
 
     public static bool CheckGUIDIsInBuffer(
         Queue<System.Guid> guidBuffer,
         System.Guid guid)
     {
-        if (guidBuffer.Contains(guid))
+        Debug.Log("Buffer:");
+        foreach(System.Guid g in guidBuffer)
+            Debug.Log(g);
+        Debug.LogFormat("Check against {0}", guid);
+        if (guidBuffer.Any(o => o.Equals(guid)))
+        {
+            Debug.Log("GUID is in Buffer");
             return false;
+        }
 
+        Debug.Log("GUID is NOT in Buffer");
         if (guidBuffer.Count >= GameSettings.guidBufferCapacity)
             guidBuffer.Dequeue();
         guidBuffer.Enqueue(guid);
         return true;
     }
 
-    public static void HitScanCheck(
+    public static bool HitScanCheck(
         Damage damage,
         Vector3 origin,
         Vector3 dir,
         float distance,
+        out RaycastHit hit,
         LayerMask mask)
     {
         Ray ray = new Ray(origin, dir);
-        RaycastHit hit;
         if(Physics.Raycast(ray, out hit, distance, mask))
         {
             IDamageable damageable = hit.collider.GetComponentInParent<IDamageable>();
@@ -60,66 +61,87 @@ public static class CombatUtils
             {
                 damageable.ApplyDamage(damage);
             }
+
+            return true;
         }
+
+        return false;
     }
 
-    public static void HitboxCheck(
+    public static bool HitboxCheck(
         Damage damage,
         Vector3 origin,
-        Vector3 dir,
-        float distance,
+        Vector3 end,
         float radius,
+        out RaycastHit[] hits,
         LayerMask mask,
         LayerMask coverMask = default)
     {
-        Ray ray = new Ray(origin, dir);
-        RaycastHit[] hits = Physics.SphereCastAll(ray, radius, distance, mask);
+        Vector3 disp = end - origin;
+        Ray ray = new Ray(origin, disp.normalized);
+        hits = Physics.SphereCastAll(ray, radius, disp.magnitude, mask);
+        bool hitConfirm = false;
         foreach(RaycastHit hit in hits)
         {
-            IDamageable damageable = hit.collider.GetComponentInParent<IDamageable>();
-            if (damageable != null)
+            if (coverMask == default)
             {
-                if(coverMask == default)
-                {
+                IDamageable damageable = hit.collider.GetComponentInParent<IDamageable>();
+                if (damageable != null)
                     damageable.ApplyDamage(damage);
-                }
-                else
+
+                hitConfirm = true;
+            }
+            else
+            {
+                Vector3 _disp = hit.point - origin;
+                if (!Physics.Raycast(
+                    origin,
+                    disp.normalized,
+                    disp.magnitude,
+                    coverMask))
                 {
-                    Vector3 disp = hit.point - origin;
-                    if (!Physics.Raycast(
-                        origin,
-                        disp.normalized,
-                        disp.magnitude,
-                        coverMask))
-                    {
+                    IDamageable damageable = hit.collider.GetComponentInParent<IDamageable>();
+                    if (damageable != null)
                         damageable.ApplyDamage(damage);
-                    }
+
+                    hitConfirm = true;
                 }
             }
         }
+
+        return hitConfirm;
     }
 
-    public static void SphereHitCheck(
+    public static void AOEHitCheck(
         Vector3 origin,
         float radius,
         LayerMask mask,
         Damage damage,
+        out Collider[] colliders,
         AnimationCurve damageFalloff = default,
         LayerMask coverMask = default)
     {
-        Collider[] colliders = Physics.OverlapSphere(origin, radius, mask);
+        colliders = Physics.OverlapSphere(origin, radius, mask);
         foreach(Collider c in colliders)
         {
+            Vector3 disp = c.transform.position - origin;
+
             IDamageable damageable = c.GetComponentInParent<IDamageable>();
             if (damageable != null)
             {
+                if (damageFalloff != null)
+                {
+                    Damage _damage = damage.Clone();
+                    float k = damageFalloff.Evaluate(disp.magnitude / radius);
+                    _damage.amount *= k;
+                }
+
                 if (coverMask == default)
                 {
                     damageable.ApplyDamage(damage);
                 }
                 else
                 {
-                    Vector3 disp = c.transform.position - origin;
                     if (!Physics.Raycast(origin, disp.normalized, disp.magnitude, coverMask))
                     {
                         damageable.ApplyDamage(damage);
--- Assets/Scripts/Utils/Damage.cs
+++ Assets/Scripts/Utils/Damage.cs
@@ -1,7 +1,8 @@
+[System.Serializable]
 public class Damage
 {
     public System.Guid guid;
-    public float amount;
+    public float amount = 10f;
     public float shieldMultiplier = 1f;
     public DamageType damageType;
 
@@ -11,9 +12,15 @@ public class Damage
         DamageType damageType)
     {
         guid = System.Guid.NewGuid();
+        UnityEngine.Debug.Log("Damage new GUID: " + guid);
         this.amount = amount;
         this.shieldMultiplier = shieldMultiplier;
         this.damageType = damageType;
+    }
+
+    public Damage Clone()
+    {
+        return new Damage(amount, shieldMultiplier, damageType);
     }
 }
 
